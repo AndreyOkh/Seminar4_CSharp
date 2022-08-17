bdd4ebf8fb39b4cb65b9a324759814c5c3ba7bdb
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-17 23:32:50 +1000

    Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.

---
+++ HomeWork_029/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+int arrayLegth = ReadLine("Введите размер массива: ");
+int[] array = new int[arrayLegth];
+FillArray(array);
+PrintArray(array);
+
+int ReadLine(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int random = new Random().Next(0, 10);
+        array[i] = random;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
