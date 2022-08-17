cc6d99a569bd32cad8689f5c2fe0964a4a6a766a
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-17 23:32:25 +1000

    Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ HomeWork_027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string number = ReadLine("Введите число: ");
+Console.WriteLine(GetSum(number));
+
+string ReadLine(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine()!;
+}
+
+int GetSum(string number)
+{
+    int sum = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        sum += Convert.ToInt32(number[i]) - 48;
+        
+    }
+    return sum;
+}
